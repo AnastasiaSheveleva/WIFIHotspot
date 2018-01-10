cea40e27745902acc9ef34e253b9f007f22b3e09
Anastasia <mbishonok@yandex.ru>
2018-01-11 00:11:24 +0300

    Initial Commit

---
+++ WiFiHotspot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WiFiHotspot
+//
+//  Created by Анастасия on 11.01.2018.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
