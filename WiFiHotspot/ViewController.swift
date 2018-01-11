0c162a31090132c9bb5d7054d23bc6e37efc5446
Anastasia <mbishonok@yandex.ru>
2018-01-12 01:07:31 +0300

    - initial commit

--- WiFiHotspot/ViewController.swift
+++ WiFiHotspot/ViewController.swift
@@ -7,17 +7,47 @@
 //
 
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func connectAction(_ sender: Any) {
+        let hotspotConfig = NEHotspotConfiguration(ssid: inputField.text!, passphrase: "AFAF123456", isWEP: false)
+        
+        NEHotspotConfigurationManager.shared.apply(hotspotConfig) {[unowned self] (error) in
+            
+            if let error = error {
+                //self.showError(error: error)
+            }
+            else {
+                self.showSuccess()
+            }
+        }
+    }
+    
+    @IBAction func disconnectAction(_ sender: Any) {
+        NEHotspotConfigurationManager.shared.removeConfiguration(forSSID: inputField.text!)
+    }
+    
+    private func showError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Darn", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func showSuccess() {
+        let alert = UIAlertController(title: "", message: "Connected", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Cool", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
 
